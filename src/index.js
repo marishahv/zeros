1a6b969ac9cb8a65c4331b70829c328a827165da
Marina <35458264+marishahv@users.noreply.github.com>
2018-03-02 18:16:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var firstDivider = 5;
+  var zeroCount = 0;
+ 
+  function calculateZeros(divider) {
+    zeroCount += Math.floor(number / divider);
+    if(Math.floor(number / divider) > 1){
+      return calculateZeros(divider * 5);
+    }
+  } 
+  calculateZeros(firstDivider);
+  return zeroCount;
 }
+
